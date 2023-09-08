db70369562def405f1bfe59c674b59ff2ec10386
Surendran <surendran1822002@gmail.com>
2023-09-08 18:58:28 +0530

    ADDED:my_personal_website_08092023

--- PersonalWebSite/MyWebPage2/Data/UserServices.cs
+++ PersonalWebSite/MyWebPage2/Data/UserServices.cs
@@ -1,4 +1,5 @@
-﻿using MyWebPage2.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using MyWebPage2.Data;
 
 namespace MyWebPage2.Data
 {
@@ -22,6 +23,11 @@ namespace MyWebPage2.Data
         public List<tblUser> GetAllUser()
         {
             List<tblUser> User = dataAccessLayer.GetAllUser().ToList();
+            return User;
+        }
+        public List<tblUserInfo> GetPersonalInfo()
+        {
+            List<tblUserInfo> User = dataAccessLayer.GetPersonalInfo().ToList();
             return User;
         }
         public bool UserCheck(tblUser checkUser )
@@ -48,6 +54,44 @@ namespace MyWebPage2.Data
         {
             dataAccessLayer.AddLike(NewLike, currentLikeCount);
         }
+        public void imagedata (string sPath) 
+        {
+            string a = "C:\\Education\\Trining\\Personal Details\\passport size (1).png";
+            byte[] data = null;
+            FileInfo fInfo = new FileInfo(a);
+            long numBytes = fInfo.Length;
+            FileStream fStream = new FileStream(a, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fStream);
+            data = br.ReadBytes((int) numBytes);
+
+            tblUserInfo tbldata = new tblUserInfo();
+
+
+            tbldata.USERID =1;
+            tbldata.USERIMAGE=data;
+            tbldata.USERBIO ="My Life My Rule";
+            tbldata.NAME ="Surendran";
+            tbldata.LASTNAME="B";
+            tbldata.BIRTHDAY =Convert.ToDateTime("18/02/2002");
+            tbldata.GENDER ="Male";
+            tbldata.EMAIL ="surendran1822002@gmail.com";
+            tbldata.PHONE =8190970043;
+            tbldata.ADDRESS ="24/33,vivegananther Street, kulalar Palayam,Bodinayakanur, Theni -625513";
+           
+
+
+            dataAccessLayer.AddPersonalInfo(tbldata);
+
+
+
+
+
+
+
+
+
+
+    }
      
 
     }
