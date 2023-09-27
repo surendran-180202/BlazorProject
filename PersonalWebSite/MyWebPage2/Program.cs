302ea86c3c875c75eda98faf7cd35b85d58ad279
Surendran <surendran1822002@gmail.com>
2023-09-27 11:09:51 +0530

    ADDED:my_personal_website_27092023

--- PersonalWebSite/MyWebPage2/Program.cs
+++ PersonalWebSite/MyWebPage2/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<UserServices>();
+builder.Services.AddSingleton<LoginShowHide>();
+
 builder.Services.AddSyncfusionBlazor();
 
 var app = builder.Build();
